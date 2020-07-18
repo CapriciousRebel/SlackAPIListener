3d5afd27ea1d2183e070263736cb3704b1a29352
Smith <codingfreak98@69FuckOfflilpp.local>
2020-07-18 16:10:01 +0530

    install boilerplate requirements

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Create Virtual Env
+python3 -m venv venv
+# Activate Virtual Env
+source venv/bin/activate
+# Update pip
+pip install --upgrade pip
+# Install dependencies
+pip install -r requirements.txt
