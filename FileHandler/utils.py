85b8d29c7e7cd62ae07940eec940d2e6cfb86b5b
Smith <codingfreak98@69FuckOfflilpp.local>
2020-07-18 23:39:55 +0530

    solve the upload issue

--- FileHandler/utils.py
+++ FileHandler/utils.py
@@ -15,6 +15,7 @@ TEMP_FILE = '/tmp/temp.jpg'
 def save_to_tmp(image_url):
     try:
         headers = {"Authorization": f"Bearer {SLACK_APP_BEARER_TOKEN}"}
+        print("Fetching the file from Slack API . . .")
         img_data = requests.get(
             image_url, headers=headers).content
     except:
@@ -25,45 +26,35 @@ def save_to_tmp(image_url):
             handler.write(img_data)
         return 1
     except:
-        print("Failed to write the file in /tmp/!")
+        print("Failed to write the file in /tmp/ !!!")
         return 0
 
 
 # Upload file from /tmp/temp.jpg to google drive
 def upload_to_drive(file_name):
-
-    print("Making a request to save file in Google Drive . . . . .")
-
     headers = {"Authorization": f"Bearer {GOOGLE_DRIVE_BEARER_TOKEN}"}
-
-    print("Headers set: ", headers)
-
     para = {
         "name": file_name,
         "parents": [DRIVE_FOLDER_ID, ]
     }
-
-    print("para set", para)
-
     files = {
         'data': ('metadata', json.dumps(para), 'application/json; charset=UTF-8'),
         'file': ('image/jpeg', open(TEMP_FILE, "rb"))
     }
 
-    print("files set", files)
-
-    print("Sending Request to drive...")
-
+    print("Sending Request to Google drive API . . . ")
     response = requests.post(
         "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
         headers=headers,
         files=files
     )
-
-    print("Response code from Google Drive API: ", response.status_code)
+    print("Response Status Code from Google Drive API: ", response.status_code)
+    if(response.status_code == 200):
+        return 1
+    return 0
 
 
 # delete file from /tmp/temp.jpg
 def delete_from_tmp():
+    print("Deleting the file from /tmp/")
     os.remove(TEMP_FILE)
-    return 1
--- FileHandler/views.py
+++ FileHandler/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 # DRF imports
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
+from rest_framework import status
 # Python imports
 import yaml
 import json
@@ -17,7 +18,7 @@ from . import utils
 def drive_backup(request):
 
     if request.method == "GET":
-        content = "Hello! Make a POST Request here to store the files to drive from slack."
+        content = "Hello! Make a POST Request on this url to store the files to drive from slack."
         response = Response(content)
         response.accepted_renderer = JSONRenderer()
         response.accepted_media_type = "application/json"
@@ -25,7 +26,9 @@ def drive_backup(request):
         return response
 
     if request.method == "POST":
+        
         print("POST Request made!")
+        
         try:
             # convert request body to a dictionary
             request_as_a_dict = yaml.safe_load(request.body.decode("utf-8"))
@@ -39,21 +42,15 @@ def drive_backup(request):
                     # store each file in /tmp/temp.jpg and then upload to drive
                     for i in range(len(file_as_a_dict['files'])):
                         image_url = file_as_a_dict['files'][i]['url_private_download']
-                        
-                        arr = os.listdir('/tmp/')
-                        print("before save to temp:", arr)
+                        # Save File from Slack API to /tmp/
 
-                        utils.save_to_tmp(image_url)
-
-                        arr = os.listdir('/tmp/')
-                        print("after save to temp:", arr)
-
-                        utils.upload_to_drive("Filename")
-
-                        arr = os.listdir('/tmp/')
-                        print("after upload to drive:", arr)
-
-                        utils.delete_from_tmp()
+                        if(utils.save_to_tmp(image_url)):
+                            if(utils.upload_to_drive("Filename")):
+                                utils.delete_from_tmp()
+                            else:
+                                return Response("Failed to Get the file from Slack, try changing the SLACK_APP_BEARER_TOKEN, or add files:read scope to it.", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                        else:
+                            return Response("Failed to Upload the file to drive, try changing the GOOGLE_DRIVE_BEARER_TOKEN, or check it's scopes. Also Check the DRIVE_FOLDER_ID", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         except:
             # Send the request as a response when slack first talk to the API with challenge parameter
             response = Response(request)
