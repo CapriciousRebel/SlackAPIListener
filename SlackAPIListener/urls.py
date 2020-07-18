cd963d63af42c4de5f6539dff8c3cf06cef91e4b
Smith <codingfreak98@69FuckOfflilpp.local>
2020-07-19 00:01:17 +0530

    change url pattern

--- SlackAPIListener/urls.py
+++ SlackAPIListener/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('Rerouter.urls')),
+    path('', include('Rerouter.urls')),
     path('FileHandler/', include('FileHandler.urls'))
 ]
