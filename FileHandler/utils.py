bfa974c3f08cea13ab236f029a6276c602c37774
Smith <codingfreak98@69FuckOfflilpp.local>
2020-07-18 23:25:46 +0530

    testing

--- FileHandler/utils.py
+++ FileHandler/utils.py
@@ -1,5 +1,6 @@
 # Python imports
 import requests
+import json
 import os
 
 
@@ -30,46 +31,36 @@ def save_to_tmp(image_url):
 
 # Upload file from /tmp/temp.jpg to google drive
 def upload_to_drive(file_name):
-    try:
-        print("Making a request to save file in Google Drive . . . . .")
 
-        headers = {"Authorization": f"Bearer {GOOGLE_DRIVE_BEARER_TOKEN}"}
+    print("Making a request to save file in Google Drive . . . . .")
 
-        print("Headers set: ", headers)
+    headers = {"Authorization": f"Bearer {GOOGLE_DRIVE_BEARER_TOKEN}"}
 
-        para = {
-            "name": file_name,
-            "parents": [DRIVE_FOLDER_ID, ]
-        }
+    print("Headers set: ", headers)
 
-        print("para set", para)
+    para = {
+        "name": file_name,
+        "parents": [DRIVE_FOLDER_ID, ]
+    }
 
-        # Error
-        try:
-            with open(TEMP_FILE, "rb") as file:
-                print("Temp file opened!")
-        except:
-            print("Couldn't open temp file!!!!")
+    print("para set", para)
 
-        # files = {
-        #    'data': ('metadata', json.dumps(para), 'application/json; charset=UTF-8'),
-        #    'file': ('image/jpeg', open(TEMP_FILE, "rb"))
-        # }
+    files = {
+        'data': ('metadata', json.dumps(para), 'application/json; charset=UTF-8'),
+        'file': ('image/jpeg', open(TEMP_FILE, "rb"))
+    }
 
-        print("files set", files)
+    print("files set", files)
 
-        print("Sending Request to drive...")
+    print("Sending Request to drive...")
 
-        response = requests.post(
-            "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
-            headers=headers,
-            files=files
-        )
+    response = requests.post(
+        "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
+        headers=headers,
+        files=files
+    )
 
-        print("Response code from Google Drive API: ", response.status_code)
-
-    except:
-        print("Failed to upload to drive!!!")
+    print("Response code from Google Drive API: ", response.status_code)
 
 
 # delete file from /tmp/temp.jpg
