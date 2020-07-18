e187a5e3997775666caf051e2661c5e642d5d389
Smith <codingfreak98@69FuckOfflilpp.local>
2020-07-19 00:19:21 +0530

    add slack auth

--- Rerouter/views.py
+++ Rerouter/views.py
@@ -10,6 +10,8 @@ import yaml
 import json
 import requests
 import os
+import hmac
+import hashlib
 # App imports
 
 
@@ -17,7 +19,6 @@ import os
 def rerouter(request):
 
     if request.method == "GET":
-        print(request.headers)
         content = "Hello! Make a POST Requests on this url with auth token from your slack app to access the services."
         response = Response(content)
         response.accepted_renderer = JSONRenderer()
@@ -27,7 +28,18 @@ def rerouter(request):
 
     if request.method == "POST":
 
-        print(request.headers)
+        body = request.get_data()
+
+        XSlackRequestTimestamp = request.headers['X-Slack-Request-Timestamp']
+
+        sig_basestring = 'v0:%s:%s' % (timestamp, body.decode('utf-8'))
+        computed_sha = hmac.new(secret,
+                                sig_basestring.encode('utf-8'),
+                                digestmod=hashlib.sha256).hexdigest()
+        my_sig = 'v0=%s' % (computed_sha,)
+
+        slack_sig = request.headers['X-Slack-Signature']
+        print(slack_sig == my_sig)
 
         response = Response(request)
         response.accepted_renderer = JSONRenderer()
